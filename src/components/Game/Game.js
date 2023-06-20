a7bb8d5517f06cdc05c1665ad7a30258b2e37ece
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2023-06-20 13:33:06 -0600

    Game Logic

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import GuessInput from '../GuessInput'
+import GuessResults from '../GuessResults'
+import GameBanner from '../GameBanner'
+import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +15,61 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const initialGuesses = [];
+  range(0, NUM_OF_GUESSES_ALLOWED).map((i) =>
+    initialGuesses.push({
+      guess: (new Array(5).fill({ letter: '', status: false })),
+      id: crypto.randomUUID()
+    }
+  ));
+
+  const [guess, setGuess] = React.useState('');
+  const [guessHistory, setGuessHistory] = React.useState(initialGuesses);
+  const [currGuess, setCurrGuess] = React.useState(0);
+  const [disabledInput, setDisabledInput] = React.useState(false);
+  const [showBanner, setShowBanner] = React.useState({
+    show: false,
+    winner: false,
+  });
+
+  function handleGuessSubmit() {
+    if (currGuess !== NUM_OF_GUESSES_ALLOWED) {
+      const historyCpy = [...guessHistory];
+      historyCpy[currGuess] = {
+        guess: checkGuess(guess, answer),
+        id: crypto.randomUUID()
+      }
+      setGuessHistory(historyCpy);
+      setCurrGuess(currGuess + 1);
+      setGuess('');
+
+      if (guess === answer) {
+        setShowBanner({
+          show: true,
+          winner: true,
+        });
+        setDisabledInput(true);
+      } else {
+        if (currGuess === NUM_OF_GUESSES_ALLOWED - 1) {
+          setShowBanner({
+            show: true,
+            winner: false,
+          });
+          setDisabledInput(true);
+        }
+      }
+    }
+  };
+  
+  return (
+    <>
+      <GuessResults guessHistory={guessHistory} />
+      <GuessInput disabledInput={disabledInput} guess={guess} setGuess={setGuess} handleGuessSubmit={handleGuessSubmit} />
+      {showBanner.show &&
+        <GameBanner winner={showBanner.winner} tries={currGuess} answer={answer} />
+      }
+    </>
+  );
 }
 
 export default Game;
